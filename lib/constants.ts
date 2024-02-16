ec12be0b6d5fc82acfd524a7183a0d156ede16c4
Ubuntu <ubuntu@ip-172-31-15-177.us-west-1.compute.internal>
2024-02-16 15:46:28 +0000

    next edit

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export type AuthType = "disabled" | "basic" | "google_oauth" | "oidc" | "saml";
 
-export const INTERNAL_URL = "https://backend-dev.folder.chat/api";
+export const INTERNAL_URL = "https://demo.folder.chat/api";
 export const NEXT_PUBLIC_DISABLE_STREAMING =
   process.env.NEXT_PUBLIC_DISABLE_STREAMING?.toLowerCase() === "true";
 
